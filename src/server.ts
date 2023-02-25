8bbdce53d1be1e2727599ff84cf784ee463ec1e2
rafaalmeida1 <rafaprof312@gmail.com>
2023-02-25 13:18:47 -0300

    fix: Cors

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,23 @@
-import Fastify from 'fastify';
-import cors from '@fastify/cors';
-import { appRoutes } from './routes';
+import Fastify from "fastify";
+import cors from "@fastify/cors";
+import { appRoutes } from "./routes";
 
 const app = Fastify();
 
+//allow cors for any url
+
 app.register(cors, {
   origin: true,
-  methods: ['GET','HEAD','PUT','PATCH','POST','DELETE'],
+  allowedHeaders: ["Access-Control-Allow-Origin", "*"],
 });
+
 app.register(appRoutes);
 
-app.listen({
-  port: 3333,
-  host: '0.0.0.0'
-}).then(() => {
-  console.log(`HTTP Server running on port http://localhost:${3333}`)
-})
+app
+  .listen({
+    port: 3333,
+    host: "0.0.0.0",
+  })
+  .then(() => {
+    console.log(`HTTP Server running on port http://localhost:${3333}`);
+  });
